e8639e2a9b4199ce0fd66594afc470c8972fb464
athirajayachandren <athirajayanvava1998@gmail.com>
2019-03-20 12:51:24 +0530

    custome

---
+++ Addmodule.js
@@ -0,0 +1,6 @@
+module.exports.Add=(a,b)=>{
+
+    var c=a+b;
+    return c;
+    
+}
---
+++ add.js
@@ -0,0 +1,6 @@
+const myadd=require('./Addmodule.js');
+var x=80;
+var y=20;
+var z= myadd.Add(x,y);
+console.log(z);
+
